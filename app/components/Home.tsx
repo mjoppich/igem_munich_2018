7da530d103a135484987a7fbe660bf28ae644fbc
RitaOlenchuk <rita@Ritas-MacBook-Pro.local>
2018-09-13 17:12:54 +0200

    background black

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -3,66 +3,75 @@ import MobileStepper from '@material-ui/core/MobileStepper';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import LinearProgress from '@material-ui/core/LinearProgress';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
-import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import { Card } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import IconButton from '@material-ui/core/IconButton';
 import Icon from '@material-ui/core/Icon';
-import SaveIcon from '@material-ui/icons/Save';
+import CardContent from '@material-ui/core/CardContent';
 
 const tutorialSteps = [
   {
-    label: 'Even if you think you did the wetlab part right, your drylab master will tell you, something is screwed up.',
+    label: 'Even if you think you did the wetlab part right, your drylab master will tell you, something is screwed up. Nanopore sequencing of bacteriophages was one of the important topics that we worked with. The analysis of the MinION sequencing data was orientated to build one consensus sequencing out of all sequences (reads) gained by sequencing of each DNA sample. This consensus sequence can also be used to determine the origin and genetic makeup of the bacteriophage. Sequencing results of five different phages (T4, T7, NES, 3S, FFP) shown high contamination by E.coli that was not seen on the gel in the lab. In order to build quality control of each sequencing and analyse rational only the data without forein DNA we provide a tool to check for contamination that does not require any data preprocessing and is in principle suitable for every sequencing method that will give standard .fastq output format. This tool is also helpful by building the consensus sequence, because there is an option to extract only sequences (reads) that were not identified as part of contamination DNA sample.',
     header: "Welcome to the contamination tool SequInto",
     imgPath: '../sequinfo_logo.jpeg',
   },
   {
-    label: '1. Work with something that you like, like…',
-    header: "WORK",
+    header: 'Sequencing data',
+    label: "Reads in fastq",
     
     content: <Button variant="contained" color="default">
     Upload
-    <CloudUploadIcon />
+    <Icon>attach_file</Icon>
   </Button>
   },
   {
-    label: '2. Keep your friends close to you and hangout with them',
-    header: "FRIENDS",
+    header: 'Contaminations',
+    label: "fasta",
     
     content: <div>
       <Card style={{marginBottom: "50px"}}>
         <Button variant="contained" color="default">
         Upload
-        <CloudUploadIcon />
+        <Icon>attach_file</Icon>
         </Button>
       </Card>
       <Card style={{marginBottom: "50px"}}>
-      <p><i>E.coli</i></p>
-      <IconButton aria-label="Delete" disabled color="primary">
-        <DeleteIcon />
-      </IconButton>
+      <CardContent>
+        <div style={{display: "inline-flex", verticalAlign: "middle", alignItems: "center"}}>
+          <span>Escherichia coli</span>
+          <IconButton aria-label="Delete" color="primary">
+          <DeleteIcon />
+          </IconButton>
+        </div>
+      </CardContent>
+
     </Card>
     <Card style={{marginBottom: "50px"}}>
-      <p>Human</p>
-      <IconButton aria-label="Delete" disabled color="primary">
+    <CardContent>
+    <div style={{display: "inline-flex", verticalAlign: "middle", alignItems: "center"}}>
+        <span>Homo Sapiens</span>
+        <IconButton aria-label="Delete" color="primary">
         <DeleteIcon />
-      </IconButton>
+        </IconButton>
+        </div>
+    </CardContent>
     </Card>
     </div>
   },
   {
-    label: '3. Travel everytime that you have a chance',
-    header: "TRAVEL"
+    header: 'Summary',
+    label: "User input summary",
+    content: <LinearProgress />
   },
   {
-    label: '4. And contribute to Material-UI :D',
-    header: "MUI",
+    header: 'Results',
+    label: "Statistics",
     content: <div>
       <Button variant="contained" size="small" >
-        <SaveIcon />
-        Save
+      Save
+      <Icon>save_alt</Icon>
       </Button>
     </div>
   },
@@ -82,7 +91,7 @@ class TextMobileStepper extends React.Component<{},{activeStep:any}> {
 
   handleBack = () => {
     this.setState(prevState => ({
-      activeStep: prevState.activeStep - 1,
+      activeStep: 0,
     }));
   };
 
@@ -92,11 +101,13 @@ class TextMobileStepper extends React.Component<{},{activeStep:any}> {
 
     return (
       <div>
-        <Card style={{marginBottom: "50px"}}>
-        <img
-          src={tutorialSteps[activeStep].imgPath} width="600" height="300"
-        />
+        <Card style={{marginBottom: "25px"}}>
+        <CardContent>
+        <div style={{display: "block", marginLeft: "auto", marginRight: "auto", width: "50%"}}>
+        { tutorialSteps[activeStep].imgPath ? <img src={tutorialSteps[activeStep].imgPath} width="600" height="300"/> : <div></div>} 
+        
         <p>{tutorialSteps[activeStep].header}</p>
+        </div>
         <MobileStepper
           steps={maxSteps}
           position="static"
@@ -114,17 +125,19 @@ class TextMobileStepper extends React.Component<{},{activeStep:any}> {
           
           backButton={
             <Button size="small" onClick={this.handleBack} disabled={activeStep === 0}>
-              <KeyboardArrowLeft />
               Reset
               <Icon>youtube_searched_for</Icon>
             </Button>
           }
         />
+        </CardContent>
         </Card>
-        <Card style={{marginBottom: "50px"}}>
+        <Card style={{marginBottom: "25px"}}>
+        <CardContent>
         <Paper square elevation={0}>
           <Typography>{tutorialSteps[activeStep].label}</Typography>
         </Paper>
+        </CardContent>
         </Card>
         {tutorialSteps[activeStep].content}
         
