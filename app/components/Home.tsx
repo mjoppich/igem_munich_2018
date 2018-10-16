d1540f3ad9f765cc57bf15fa0d7748ba5bada35f
Julia Mayer <juliamayer@me.com>
2018-10-16 17:24:02 +0200

    CHG Warning freeze of app

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -629,32 +629,35 @@ class TextMobileStepper extends React.Component<{}, {
             ,
         
         alarmlabel:
-        <div
-            style={{
-                display: "inline-flex",
-                verticalAlign: "middle",
-                alignItems: "center" }}>
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24">
-                <path d="M9 21c0 .55.45 1 1 1h4c.55 0 1-.45 1-1v-1H9v1zm3-19C8.14 2 5 5.14 5 9c0 2.38 1.19 4.47 3 5.74V17c0 .55.45 1 1 1h6c.55 0 1-.45 1-1v-2.26c1.81-1.27 3-3.36 3-5.74 0-3.86-3.14-7-7-7zm2.85 11.1l-.85.6V16h-4v-2.3l-.85-.6C7.8 12.16 7 10.63 7 9c0-2.76 2.24-5 5-5s5 2.24 5 5c0 1.63-.8 3.16-2.15 4.1z" />
-            </svg>
-                
+            <div
+                style={{
+                    verticalAlign: "middle",
+                    alignItems: "center" }}>
 
-                <div style={{display: "inline-flex"}}>
-                        <Typography component="span">Choose your FastA files.&nbsp;</Typography>
-                        <Typography component="span"> </Typography>
-                        <Typography component="span" color="secondary"> 
-                            Calculations can take several minutes to complete!
+
+                    <Typography component="span" color="secondary">
+                        Calculations can take several minutes to complete, during that the app might become not responsible.
+                    </Typography>
+
+
+                    <div 
+                        style={{
+                            verticalAlign: "middle",
+                            display: "inline-flex"}}>
+
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24">
+                            <path d="M9 21c0 .55.45 1 1 1h4c.55 0 1-.45 1-1v-1H9v1zm3-19C8.14 2 5 5.14 5 9c0 2.38 1.19 4.47 3 5.74V17c0 .55.45 1 1 1h6c.55 0 1-.45 1-1v-2.26c1.81-1.27 3-3.36 3-5.74 0-3.86-3.14-7-7-7zm2.85 11.1l-.85.6V16h-4v-2.3l-.85-.6C7.8 12.16 7 10.63 7 9c0-2.76 2.24-5 5-5s5 2.24 5 5c0 1.63-.8 3.16-2.15 4.1z" />
+                        </svg>
+
+                        <Typography component="span">
+                            Choose your FastA files.&nbsp;
                         </Typography>
-                </div>
-
-
-
-
-
+                            
+                    </div>
             </div>,
 
         topimgPath: 'sequinfo_neg.jpg',
@@ -750,22 +753,38 @@ class TextMobileStepper extends React.Component<{}, {
 
             </div>,
 
-        alarmlabel:
-        <div
-            style={{
-                display: "inline-flex",
-                verticalAlign: "middle",
-                alignItems: "center" }}>
 
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24">
-                <path d="M9 21c0 .55.45 1 1 1h4c.55 0 1-.45 1-1v-1H9v1zm3-19C8.14 2 5 5.14 5 9c0 2.38 1.19 4.47 3 5.74V17c0 .55.45 1 1 1h6c.55 0 1-.45 1-1v-2.26c1.81-1.27 3-3.36 3-5.74 0-3.86-3.14-7-7-7zm2.85 11.1l-.85.6V16h-4v-2.3l-.85-.6C7.8 12.16 7 10.63 7 9c0-2.76 2.24-5 5-5s5 2.24 5 5c0 1.63-.8 3.16-2.15 4.1z" />
-            </svg>
-            You will find all figures and saved files in your chosen output directory
-        </div>,
+            alarmlabel:
+            <div
+                style={{
+                    verticalAlign: "middle",
+                    alignItems: "center" }}>
+
+
+                    <Typography component="span" color="secondary">
+                        Saving can take several minutes to complete, during that the app might become not responsible.
+                    </Typography>
+
+
+                    <div 
+                        style={{
+                            verticalAlign: "middle",
+                            display: "inline-flex"}}>
+
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24">
+                            <path d="M9 21c0 .55.45 1 1 1h4c.55 0 1-.45 1-1v-1H9v1zm3-19C8.14 2 5 5.14 5 9c0 2.38 1.19 4.47 3 5.74V17c0 .55.45 1 1 1h6c.55 0 1-.45 1-1v-2.26c1.81-1.27 3-3.36 3-5.74 0-3.86-3.14-7-7-7zm2.85 11.1l-.85.6V16h-4v-2.3l-.85-.6C7.8 12.16 7 10.63 7 9c0-2.76 2.24-5 5-5s5 2.24 5 5c0 1.63-.8 3.16-2.15 4.1z" />
+                        </svg>
+
+                        <Typography component="span">
+                            You will find all figures and saved files in your chosen output directory.
+                        </Typography>
+                            
+                    </div>
+            </div>,
 
         topimgPath: 'sequinfo_neg.jpg',
 
